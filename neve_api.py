2b5826a62d8c044e9168ca5cb5ccc86fb5cfdd9d
NAVNAV123 <52596137+NAVNAV123@users.noreply.github.com>
2019-12-09 22:51:49 +0200

    Need to connect to request to the code

--- neve_api.py
+++ neve_api.py
@@ -2,14 +2,15 @@ import requests
 import csv
 from random import randint
 
-# rand_temp = randint(-30, 30)
-# city = input("Wanted city ")
-# if str.isdigit(city):
-#     print("Wrong city")
-# else:
-#     print("Thank you!")
-#     print("the temp is %s" %rand_temp)
+# #POST request
+# #TODO: connect the request to the code.
+# dictToSend = {'question': 'what is the answer?'}
+# resp = requests.post('http://localhost:5000/post/endpoint', json = dictToSend)
+# print ('response from server: ',resp.text)
+# dictFromServer = resp.json()
 
+
+#read current CSV file
 with open('city_temp.csv', mode='r') as csv_file:
     csv_reader = csv.DictReader(csv_file)
     line_count = 0
@@ -17,6 +18,26 @@ with open('city_temp.csv', mode='r') as csv_file:
         if line_count == 0:
             print('Column names are %s' % ", ".join(row))
             line_count += 1
-        print('\t%s have %s temp, mode: %s.' % (row["city"], row["temp"], row["mode"]))
+        print('\t%s have %s temp.' % (row["city"], row["temp"]))
         line_count += 1
     print('Proccessed %s lines.' % (line_count))
+
+
+rand_temp = randint(-30, 30)
+city = input("Wanted city ")
+if str.isdigit(city):#validation
+    print("Wrong city")
+else:
+    print("Thank you!")
+    print("the temp is %s" %rand_temp)
+
+
+#write CSV file
+with open('city_temp.csv', mode='w') as csv_file:
+    fieldnames = ['city', 'temp']
+    writer = csv.DictWriter(csv_file, fieldnames = fieldnames)
+
+    writer.writeheader()
+    writer.writerow({'city': city,
+                     'temp': rand_temp,
+                     })
