48364472b5981275fa3bf9e3bc47f1ef80f3b6fb
NAVNAV123 <52596137+NAVNAV123@users.noreply.github.com>
2020-01-21 02:38:28 +0200

    NEW

--- app.py
+++ app.py
@@ -1,35 +1,26 @@
 import requests
-from flask import Flask, render_template
-# from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# app.config[SQLALCHEMY_DATABASE_URI] = 'sqlite:///C:\\sqlite\\weather.db'
-# db = SQLAlchemy(app)
-
-# class city(db.model):
-# 	id = db.Column(db.Integer, primary_key = True)
-# 	name = db.Column(db.String(50), nullable = False)
-
-
-@app.route('/')
+@app.route('/temp', methods = ['GET', 'POST'])
 def home():
-	url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=70aea3443f94204bfc4c559b45f12876'
-	city = 'Las Vegas'
-
-	r = requests.get(url.format(city)).json()
-	print(r)
+	new_city = request.form['city']
+	r = requests.get('https://api.openweathermap.org/data/2.5/weather?q='+new_city+'&units=metric&appid=70aea3443f94204bfc4c559b45f12876')
+	json_obj = r.json()
 
 	weather = {
-		'city': city,
+		'city' : new_city,
 		'temperture': r['main']['temp'],
 		'description': r['weather'][0]['description'],
 		'icon': r['weather'][0]['icon'],
 	}
 
-	print(weather)
-
 	return render_template('weather.html', weather = weather)
+
+@app.route('/')
+def index():
+	return render_template('index.html')
 
 if __name__ == "__main__":
 	app.run(debug = True)
